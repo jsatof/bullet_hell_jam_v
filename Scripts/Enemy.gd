1b89a7597977603c5995ed745f784b1ef0e35a56
TerraReef27 <terrareef27@gmail.com>
2024-05-13 20:50:50 -0400

    Enemy pathing improvement

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -7,10 +7,12 @@ var movement_targets = []
 
 var current_target: int = 0
 var current_time: float = 0.0
+# var move_tween: Tween
 
 @onready var globals = get_node("/root/GlobalState")
 
 func _ready():
+	# move_tween = get_tree().create_tween().set_loops()
 	current_time = 0.0
 	current_target = 0
 	move_to_targets()
@@ -21,24 +23,18 @@ func _process(delta):
 	if current_time >= lifetime:
 		remove_self()
 
-# Due to the need to await the pause I had to go with this solution
-# The more elegent for each loop was not viable to my knowledge
 func move_to_targets():
-	var t = movement_targets[current_target]
-	var move_tween = get_tree().create_tween()
-	move_tween.tween_property(self,
-		"position",
-		t.point,
-		1).set_trans(t.trans)
-	move_tween.tween_callback(next_position.bind(t.pause))
+	for t in movement_targets:
+		# Make a new tween each time because there was issues reusing one...sad
+		var move_tween = get_tree().create_tween()
+		move_tween.tween_property(self,
+			"position",
+			t.point,
+			1).set_trans(t.trans)
+		await move_tween.tween_interval(t.pause).finished
+		move_tween.kill()
 
-func next_position(time: float):
-	await get_tree().create_timer(time).timeout
-	current_target += 1
-	if current_target >= movement_targets.size():
-		remove_self()
-	else:
-		move_to_targets()
+	remove_self()
 
 func remove_self():
 	print("dead")
