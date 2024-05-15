ca0f3fabbbb1803a858337179505e658dd4c85db
James Ferrarelli <36771190+jsatof@users.noreply.github.com>
2024-05-14 21:08:24 -0400

    Simple shop UI (#4)
    * Add global singleton for game state data
    
    * Enabled the vim plugin in the addons/ dir
    
    * Stock Market ui Widget
    
    In the level ui scene, create and add a widget that ticks as new stock
    values update.
    Displays the graph, draws the lines, and shows the price value along
    with a percent differential between the previous price value.
    
    * Add test buttons to interact with the stock market
    
    A "Buy Share" button to use funds to purchase a stock share.
    A "Sell Share" button to sell a share of an owned stock and add funds
    back to inventory.
    
    * Create shop
    
    Added a simple shop that displays information about a gun and an option
    to purchase. The test button buys the weapon and should update the
    bullet cost and equipped weapon label
    
    * Shop UX/UI update plus refactor
    
    The test buttons are functional. Buy a gun from the shop if funds are
    available. The button is disabled when there aren't enough funds.
    
    Refactor Items:
    	- Collapse code blocks into simpler
    	- Make guns Dictionary types rather than Array
    	- Add detection for which gun fired to bill the player properly

--- Scripts/StockGraphGraph.gd
+++ Scripts/StockGraphGraph.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var price_label = get_parent().get_node("StockPriceLabel")
 @onready var price_diff_label = get_parent().get_node("StockDiffLabel")
 @onready var color_rect = get_parent().get_node("GraphBGColorRect")
+@onready var timer := get_parent().get_node("Timer")
+
 
 var stock_points: PackedFloat32Array
 
@@ -16,21 +18,24 @@ func _ready():
 	assert(price_label)
 	assert(price_diff_label)
 	assert(color_rect)
+	assert(timer)
 
 func _draw():
 	for i in range(1, len(stock_points)):
-		var start := Vector2(point_size + (i - 1) * color_rect.size.x / 8, stock_points[i - 1])
-		var current := Vector2(point_size + i * color_rect.size.x / 8, stock_points[i])
+		var start: Vector2 = screen_coord_from_stock_points(i - 1)
+		var current: Vector2 = screen_coord_from_stock_points(i)
 		draw_line(start, current, Color.WEB_GREEN, 1, true)
 	# draw circle at last point
 	if len(stock_points) > 0:
 		var last_index := len(stock_points) - 1
-		draw_circle(Vector2(point_size + last_index * color_rect.size.x / 8, stock_points[last_index]), point_size, Color.WEB_GREEN)
+		draw_circle(screen_coord_from_stock_points(last_index), point_size, Color.WEB_GREEN)
+
 
 # every second the stock prices will update
 # so far only the last 10 points are saved,
 # the oldest will be evicted when a new one comes in if full
 func _on_timer_timeout():
+	print("Current Stock Price: $%.2f" % global.heckler_stock_price)
 	var current_price: float = global.heckler_stock_price
 	var percent_diff: float = global.heckler_percent_diff
 
@@ -45,11 +50,20 @@ func _on_timer_timeout():
 	var new_price_point: float = y_start_pos - scaling * current_price
 	stock_points.push_back(new_price_point)
 
+	# Test code for interacting with stock market
+	if current_price > 70.0:
+		print("Current Stock Price: ", current_price)
+		timer.stop()
+
 	if len(stock_points) >= 10:
 		stock_points = stock_points.slice(1, 10)
 
 	global.set_new_heckler_stock_price()
 	queue_redraw()
 
-
+func screen_coord_from_stock_points(index: int) -> Vector2:
+	# This condition should never occur, but just in case
+	if index < 0 || index >= len(stock_points):
+		return Vector2(-1000, -1000) # will lead to draw dramatically wild line
+	return Vector2(point_size + index * color_rect.size.x / 8, stock_points[index])
 
