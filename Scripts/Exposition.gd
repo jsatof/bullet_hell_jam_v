e59020da3d1ccec69152c253b7c6546efac444cd
James Ferrarelli <36771190+jsatof@users.noreply.github.com>
2024-05-19 23:25:59 -0400

    Add exposition screen to give keybinds and brief context (#23)

---
+++ Scripts/Exposition.gd
@@ -0,0 +1,44 @@
+extends Control
+
+@onready var audio_manager := get_node("/root/AudioManager")
+@onready var global := get_node("/root/GlobalState")
+
+@onready var collect_label := $CollectLabel
+@onready var buy_weapons_label := $BuyWeaponsLabel
+@onready var make_investments_label := $MakeInvestmentsLabel
+@onready var payback_label := $PaybackLabel
+@onready var continue_label := $ContinueLabel
+@onready var timer := $Timer
+
+var label_list: Array
+var ready_to_continue := false
+
+func _ready() -> void:
+	audio_manager.add_soundtrack_lpf()
+
+	# these must be in order
+	# and set to NOT visible in the scene
+	label_list = [
+		collect_label,
+		buy_weapons_label,
+		make_investments_label,
+		payback_label,
+		continue_label,
+	]
+
+	timer.timeout.connect(on_timer_timeout)
+	timer.wait_time = 2
+	timer.start()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("buy_gun") && ready_to_continue:
+		audio_manager.remove_soundtrack_lpf()
+		global.start_new_game()
+
+func on_timer_timeout() -> void:
+	if len(label_list) > 0:
+		label_list.front().set_visible(true)
+		label_list = label_list.slice(1, len(label_list))
+	else:
+		ready_to_continue = true
+		timer.stop()
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -6,12 +6,9 @@ extends Control
 func _ready() -> void:
 	var main_menu_song := AudioStreamOggVorbis.load_from_file("res://Resources/Audio/Music/Reonidas.ogg")
 	audio_manager.set_soundtrack(main_menu_song)
-	audio_manager.add_soundtrack_lpf()
 	audio_manager.play_soundtrack()
 
 func _input(event: InputEvent) -> void:
 	if(event.is_action_pressed("buy_gun")):
-		start()
-
-func start() -> void:
-	globals.start_new_game()
+		var next_scene := preload("res://Scenes/Exposition.tscn")
+		get_tree().change_scene_to_packed(next_scene)
